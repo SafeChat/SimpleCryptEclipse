a5037ee1c2cf03b1ee2dd2c3354d73946eaaa839
Zakant <zakant@gmx.de>
2014-03-23 13:35:41 +0100

    Implement first overloading

--- src/de/zakath/simplecrypt/PBKDF2.java
+++ src/de/zakath/simplecrypt/PBKDF2.java
@@ -1,22 +1,29 @@
 package de.zakath.simplecrypt;
 
 import java.security.*;
-import java.security.spec.KeySpec;
+import java.security.spec.*;
 
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.*;
+import javax.crypto.spec.*;
 
 public class PBKDF2
 {
+			
+	public static Key deriveKey(char[] password, byte[] salt, int keysize)
+			throws InvalidKeySpecException, NoSuchAlgorithmException
+	{
+		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+		KeySpec ks = new PBEKeySpec(password, salt, 128, keysize);
+		SecretKey s = f.generateSecret(ks);
+		Key k = new SecretKeySpec(s.getEncoded(), "AES");
+		return k;
+	}
 
-	public Key deriveKey(byte[] password, byte[] salt, int keysize)
+	public static Key deriveKey(char[] password, int keysize)
 	{
 
 	}
 
-	public Key deriveKey(byte[] password, int keysize)
-	{
-
-	}
-
+	
+	
 }

2cb46110e7d770f7bbb03ce7dd7f52d6a935f158
Zakant <zakant@gmx.de>
2014-03-23 13:42:26 +0100

    Implement second overloading and fixed formatting

--- src/de/zakath/simplecrypt/PBKDF2.java
+++ src/de/zakath/simplecrypt/PBKDF2.java
@@ -9,7 +9,7 @@ import javax.crypto.spec.*;
 
 public class PBKDF2
 {
-			
+
 	public static Key deriveKey(char[] password, byte[] salt, int keysize)
 			throws InvalidKeySpecException, NoSuchAlgorithmException
 	{
@@ -21,18 +21,26 @@ public class PBKDF2
 	}
 
 	public static Key deriveKey(char[] password, int keysize)
+			throws NoSuchAlgorithmException, UnsupportedEncodingException,
+			InvalidKeySpecException
 	{
-
+		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+		KeySpec ks = new PBEKeySpec(password, computeSalt(password), 128,
+				keysize);
+		SecretKey s = f.generateSecret(ks);
+		Key k = new SecretKeySpec(s.getEncoded(), "AES");
+		return k;
 	}
 
-	private static byte[] computeSalt(char[] password) throws UnsupportedEncodingException
+	private static byte[] computeSalt(char[] password)
+			throws UnsupportedEncodingException
 	{
 		byte[] salt = new String(password).getBytes("UTF-8");
-		for(int i = 0;i< password.length;i++ )
+		for (int i = 0; i < password.length; i++)
 		{
 			salt = SHA256.computeHash(salt);
 		}
 		return salt;
 	}
-	
+
 }

12a72895b6e652fd618847bca61e1f3ba0941f27
Zakant <zakant@gmx.de>
2014-03-23 13:20:10 +0100

    Fixed a formatting mistake

--- src/de/zakath/simplecrypt/SHA256.java
+++ src/de/zakath/simplecrypt/SHA256.java
@@ -2,17 +2,17 @@ package de.zakath.simplecrypt;
 
 import java.security.*;
 
-public class SHA256 
+public class SHA256
 {
 
 	public static byte[] computeHash(byte[] input)
 	{
 		try
-        {
-            return MessageDigest.getInstance("SHA-256").digest(input);
-        } catch(NoSuchAlgorithmException e)
-        {
-            return null;
-        }
+		{
+			return MessageDigest.getInstance("SHA-256").digest(input);
+		} catch (NoSuchAlgorithmException e)
+		{
+			return null;
+		}
 	}
 }

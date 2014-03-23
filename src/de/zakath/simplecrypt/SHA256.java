c0b2a88abea77dee7f464510832a679f942593d5
Zakant <zakant@gmx.de>
2014-03-23 13:14:27 +0100

    Implemented method "computeHash"

--- src/de/zakath/simplecrypt/SHA256.java
+++ src/de/zakath/simplecrypt/SHA256.java
@@ -1,10 +1,18 @@
 package de.zakath.simplecrypt;
 
+import java.security.*;
+
 public class SHA256 
 {
 
 	public static byte[] computeHash(byte[] input)
 	{
-		
+		try
+        {
+            return MessageDigest.getInstance("SHA-256").digest(input);
+        } catch(NoSuchAlgorithmException e)
+        {
+            return null;
+        }
 	}
 }

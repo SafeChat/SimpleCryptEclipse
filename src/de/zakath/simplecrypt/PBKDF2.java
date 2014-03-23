113e723b02ca04d9a22ec32b1539713a9c2808a0
Zakant <zakant@gmx.de>
2014-03-23 13:47:15 +0100

    Moved error handling back in PBKDF2.java

--- src/de/zakath/simplecrypt/PBKDF2.java
+++ src/de/zakath/simplecrypt/PBKDF2.java
@@ -11,25 +11,38 @@ public class PBKDF2
 {
 
 	public static Key deriveKey(char[] password, byte[] salt, int keysize)
-			throws InvalidKeySpecException, NoSuchAlgorithmException
 	{
-		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-		KeySpec ks = new PBEKeySpec(password, salt, 128, keysize);
-		SecretKey s = f.generateSecret(ks);
-		Key k = new SecretKeySpec(s.getEncoded(), "AES");
-		return k;
+		try
+		{
+			SecretKeyFactory f = SecretKeyFactory
+					.getInstance("PBKDF2WithHmacSHA1");
+			KeySpec ks = new PBEKeySpec(password, salt, 128, keysize);
+			SecretKey s = f.generateSecret(ks);
+			Key k = new SecretKeySpec(s.getEncoded(), "AES");
+			return k;
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException ex)
+		{
+			return null;
+		}
 	}
 
 	public static Key deriveKey(char[] password, int keysize)
-			throws NoSuchAlgorithmException, UnsupportedEncodingException,
-			InvalidKeySpecException
+
 	{
-		SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-		KeySpec ks = new PBEKeySpec(password, computeSalt(password), 128,
-				keysize);
-		SecretKey s = f.generateSecret(ks);
-		Key k = new SecretKeySpec(s.getEncoded(), "AES");
-		return k;
+		try
+		{
+			SecretKeyFactory f = SecretKeyFactory
+					.getInstance("PBKDF2WithHmacSHA1");
+			KeySpec ks = new PBEKeySpec(password, computeSalt(password), 128,
+					keysize);
+			SecretKey s = f.generateSecret(ks);
+			Key k = new SecretKeySpec(s.getEncoded(), "AES");
+			return k;
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException | UnsupportedEncodingException ex)
+		{
+			return null;
+		}
+
 	}
 
 	private static byte[] computeSalt(char[] password)

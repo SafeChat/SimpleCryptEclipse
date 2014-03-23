02b6833b37e7ea009334354e1cbda2b425716bcf
Zakant <zakant@gmx.de>
2014-03-23 13:30:42 +0100

    Added method bodys "deriveKey"

--- src/de/zakath/simplecrypt/PBKDF2.java
+++ src/de/zakath/simplecrypt/PBKDF2.java
@@ -1,6 +1,22 @@
 package de.zakath.simplecrypt;
 
+import java.security.*;
+import java.security.spec.KeySpec;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
 public class PBKDF2
 {
+
+	public Key deriveKey(byte[] password, byte[] salt, int keysize)
+	{
+
+	}
+
+	public Key deriveKey(byte[] password, int keysize)
+	{
+
+	}
 
 }
